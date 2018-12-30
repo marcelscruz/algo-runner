1e35262215bd0d1ceac2b270cb66d4014b04c59a
Marcel Cruz <marcelcruz@live.com>
2018-12-30 16:57:24 +0100

    Add comments and prototype of web worker timeout

--- src/web-worker/worker.js
+++ src/web-worker/worker.js
@@ -10,10 +10,19 @@ export default code => {
     }
     worker = new Worker(sandbox)
 
+    // Send code to worker
     worker.postMessage(code)
 
+    // Receive result from worker
     worker.onmessage = function(e) {
       console.log(e.data)
     }
+
+    // Kill worker to prevent unnecessary use of processing
+    // setTimeout(function() {
+    //   console.log('killing worker 🔪')
+    //   worker.terminate()
+    //   worker = null
+    // }, 2000)
   }
 }
